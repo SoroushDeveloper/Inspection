90dc87f1f32b26caff3c391402c994bf92f46f4a
Programmer01 <snb.programmer001@gmail.com>
2024-07-02 16:56:15 +0330

    Added UserInfoEdit Section in Home

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -12,34 +12,62 @@ use App\Models\State;
 use App\Models\Subject;
 use App\Models\Type;
 use App\Models\User;
+use Illuminate\Validation\Rule;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Home extends Component
 {
-    public $users = 0;
-    public $inspections = 0;
-    public $institutions = 0;
-    public $states = 0;
-    public $majors = 0;
-    public $types = 0;
-    public $properties = 0;
-    public $commons = 0;
-    public $consistencies = 0;
-    public $subjects = 0;
+    public $count = [
+        'users' => '---',
+        'inspections' => '---',
+        'institutions' => '---',
+        'states' => '---',
+        'majors' => '---',
+        'types' => '---',
+        'properties' => '---',
+        'commons' => '---',
+        'consistencies' => '---',
+        'subjects' => '---',
+    ];
+
+    public $form = [
+        'first_name' => '',
+        'last_name' => '',
+        'email' => '',
+        'password' => '',
+        'personal_code' => '',
+    ];
 
     public function mount()
     {
-        $this->users = User::count();
-        $this->inspections = Inspection::count();
-        $this->institutions = Institution::count();
-        $this->states = State::count();
-        $this->majors = Major::count();
-        $this->types = Type::count();
-        $this->properties = Property::count();
-        $this->commons = Common::count();
-        $this->consistencies = Consistency::count();
-        $this->subjects = Subject::count();
+        $this->count['users'] = User::count();
+        $this->count['inspections'] = Inspection::count();
+        $this->count['institutions'] = Institution::count();
+        $this->count['states'] = State::count();
+        $this->count['majors'] = Major::count();
+        $this->count['types'] = Type::count();
+        $this->count['properties'] = Property::count();
+        $this->count['commons'] = Common::count();
+        $this->count['consistencies'] = Consistency::count();
+        $this->count['subjects'] = Subject::count();
+        $this->form['first_name'] = auth()->user()->first_name;
+        $this->form['last_name'] = auth()->user()->last_name;
+        $this->form['email'] = auth()->user()->email;
+        $this->form['personal_code'] = auth()->user()->personal_code;
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'form.first_name' => 'required|max:255',
+            'form.last_name' => 'required|max:255',
+            'form.password' => 'nullable',
+            'form.email' => $this->form['email'] == auth()->user()->email ? 'required|email' : 'required|email|unique:users,email',
+            'form.personal_code' => $this->form['personal_code'] == auth()->user()->personal_code ? 'required' : 'required|unique:users,personal_code',
+        ]);
+//        auth()->user()->update($this->form);
+        return $this->redirect('/Home');
     }
 
     #[Title('میز کار')]
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -8,69 +8,63 @@
         </h2>
         <hr>
         <div class="grid md:grid-cols-5 md:gap-6 mt-5">
-            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3">
+            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3 hover:shadow-2xl">
                 <svg class="w-14 h-14 transition duration-75"
                      aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                     <path
                         d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
                 </svg>
-                <h3 class="mt-2 text-xl">
+                <a class="mt-2 text-xl hover:underline" href="{{ route('Users') }}">
                     کاربران
-                </h3>
+                </a>
                 <span
                     class="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full mt-2">
-                    {{ $users }}
+                    {{ $count['users'] }}
                 </span>
-                {{--                <p class="mt-3 px-1 bg-gray-400 dark:bg-gray-600 rounded">--}}
-                {{--                </p>--}}
             </div>
-            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3">
+            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3 hover:shadow-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                      class="bi bi-binoculars-fill w-14 h-14" viewBox="0 0 16 16">
                     <path
                         d="M4.5 1A1.5 1.5 0 0 0 3 2.5V3h4v-.5A1.5 1.5 0 0 0 5.5 1zM7 4v1h2V4h4v.882a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V13H9v-1.5a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5V13H1V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V4zM1 14v.5A1.5 1.5 0 0 0 2.5 16h3A1.5 1.5 0 0 0 7 14.5V14zm8 0v.5a1.5 1.5 0 0 0 1.5 1.5h3a1.5 1.5 0 0 0 1.5-1.5V14zm4-11H9v-.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5z"/>
                 </svg>
-                <h3 class="mt-2 text-xl">
+                <a class="mt-2 text-xl hover:underline" href="{{ route('Inspections') }}">
                     بازرسی ها
-                </h3>
+                </a>
                 <span
                     class="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full mt-2">
-                    {{ $inspections }}
+                    {{ $count['inspections'] }}
                 </span>
             </div>
-            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3">
+            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3 hover:shadow-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                      class="bi bi-buildings-fill w-14 h-14 transition duration-75" viewBox="0 0 16 16">
                     <path
                         d="M15 .5a.5.5 0 0 0-.724-.447l-8 4A.5.5 0 0 0 6 4.5v3.14L.342 9.526A.5.5 0 0 0 0 10v5.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V14h1v1.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5zM2 11h1v1H2zm2 0h1v1H4zm-1 2v1H2v-1zm1 0h1v1H4zm9-10v1h-1V3zM8 5h1v1H8zm1 2v1H8V7zM8 9h1v1H8zm2 0h1v1h-1zm-1 2v1H8v-1zm1 0h1v1h-1zm3-2v1h-1V9zm-1 2h1v1h-1zm-2-4h1v1h-1zm3 0v1h-1V7zm-2-2v1h-1V5zm1 0h1v1h-1z"/>
                 </svg>
-                <h3 class="mt-2 text-xl">
+                <a class="mt-2 text-xl hover:underline" href="{{ route('Institutions') }}">
                     آموزشگاه ها
-                </h3>
+                </a>
                 <span
                     class="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full mt-2">
-                    {{ $institutions }}
+                    {{ $count['institutions'] }}
                 </span>
-                {{--                <p class="mt-3 px-1 bg-gray-400 dark:bg-gray-600 rounded">--}}
-                {{--                </p>--}}
             </div>
-            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3">
+            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3 hover:shadow-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                      class="bi bi-map-fill w-14 h-14 transition duration-75" viewBox="0 0 16 16">
                     <path fill-rule="evenodd"
                           d="M16 .5a.5.5 0 0 0-.598-.49L10.5.99 5.598.01a.5.5 0 0 0-.196 0l-5 1A.5.5 0 0 0 0 1.5v14a.5.5 0 0 0 .598.49l4.902-.98 4.902.98a.5.5 0 0 0 .196 0l5-1A.5.5 0 0 0 16 14.5zM5 14.09V1.11l.5-.1.5.1v12.98l-.402-.08a.5.5 0 0 0-.196 0zm5 .8V1.91l.402.08a.5.5 0 0 0 .196 0L11 1.91v12.98l-.5.1z"/>
                 </svg>
-                <h3 class="mt-2 text-xl">
+                <a class="mt-2 text-xl hover:underline" href="{{ route('States') }}">
                     مناطق
-                </h3>
+                </a>
                 <span
                     class="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full mt-2">
-                    {{ $states }}
+                    {{ $count['states'] }}
                 </span>
-                {{--                <p class="mt-3 px-1 bg-gray-400 dark:bg-gray-600 rounded">--}}
-                {{--                </p>--}}
             </div>
-            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3">
+            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3 hover:shadow-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                      class="bi bi-mortarboard-fill w-14 h-14 transition duration-75" viewBox="0 0 16 16">
                     <path
@@ -78,19 +72,17 @@
                     <path
                         d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z"/>
                 </svg>
-                <h3 class="mt-2 text-xl">
+                <a class="mt-2 text-xl hover:underline" href="{{ route('Majors') }}">
                     رشته های تحصیلی
-                </h3>
+                </a>
                 <span
                     class="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full mt-2">
-                    {{ $majors }}
+                    {{ $count['majors'] }}
                 </span>
-                {{--                <p class="mt-3 px-1 bg-gray-400 dark:bg-gray-600 rounded">--}}
-                {{--                </p>--}}
             </div>
         </div>
         <div class="grid md:grid-cols-5 md:gap-6 mt-5">
-            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3">
+            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3 hover:shadow-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                      class="bi bi-spellcheck w-14 h-14 transition duration-75" viewBox="0 0 16 16">
                     <path
@@ -98,30 +90,30 @@
                     <path
                         d="M14.469 9.414a.75.75 0 0 1 .117 1.055l-4 5a.75.75 0 0 1-1.116.061l-2.5-2.5a.75.75 0 1 1 1.06-1.06l1.908 1.907 3.476-4.346a.75.75 0 0 1 1.055-.117"/>
                 </svg>
-                <h3 class="mt-2 text-xl">
+                <a class="mt-2 text-xl hover:underline" href="{{ route('Types') }}">
                     انواع واحد های آموزشی
-                </h3>
+                </a>
                 <span
                     class="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full mt-2">
-                    {{ $types }}
+                    {{ $count['types'] }}
                 </span>
             </div>
-            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3">
+            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3 hover:shadow-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                      class="bi bi-tools w-14 h-14 transition duration-75"
                      viewBox="0 0 16 16">
                     <path
                         d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3q0-.405-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708M3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026z"/>
                 </svg>
-                <h3 class="mt-2 text-xl">
+                <a class="mt-2 text-xl hover:underline" href="{{ route('Properties') }}">
                     ویژگی های فنی
-                </h3>
+                </a>
                 <span
                     class="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full mt-2">
-                    {{ $properties }}
+                    {{ $count['properties'] }}
                 </span>
             </div>
-            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3">
+            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3 hover:shadow-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                      class="bi bi-card-checklist w-14 h-14 transition duration-75" viewBox="0 0 16 16">
                     <path
@@ -129,42 +121,98 @@
                     <path
                         d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0"/>
                 </svg>
-                <h3 class="mt-2 text-xl">
+                <a class="mt-2 text-xl hover:underline" href="{{ route('Commons') }}">
                     ویژگی های غیر فنی
-                </h3>
+                </a>
                 <span
                     class="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full mt-2">
-                    {{ $commons }}
+                    {{ $count['commons'] }}
                 </span>
             </div>
-            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3">
+            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3 hover:shadow-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                      class="bi bi-shield-fill-check w-14 h-14 transition duration-75" viewBox="0 0 16 16">
                     <path fill-rule="evenodd"
                           d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793z"/>
                 </svg>
-                <h3 class="mt-2 text-xl">
+                <a class="mt-2 text-xl hover:underline" href="{{ route('Consistencies') }}">
                     سطوح پیگیری
-                </h3>
+                </a>
                 <span
                     class="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full mt-2">
-                    {{ $consistencies }}
+                    {{ $count['consistencies'] }}
                 </span>
             </div>
-            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3">
+            <div class="flex items-center flex-col bg-gray-300 dark:bg-gray-700 rounded pt-5 pb-3 hover:shadow-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                      class="bi bi-chat-text-fill w-14 h-14 transition duration-75" viewBox="0 0 16 16">
                     <path
                         d="M16 8c0 3.866-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7M4.5 5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1z"/>
                 </svg>
-                <h3 class="mt-2 text-xl">
+                <a class="mt-2 text-xl hover:underline" href="{{ route('Subjects') }}">
                     موضوعات بازدید
-                </h3>
+                </a>
                 <span
                     class="inline-flex items-center justify-center w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full mt-2">
-                    {{ $subjects }}
+                    {{ $count['subjects'] }}
                 </span>
             </div>
         </div>
+    </div>
+    <div class="bg-gray-200 dark:bg-gray-800 mt-5 p-5 rounded">
+        <h2 class="text-center text-3xl mb-5">
+            ویرایش اطلاعات کاربری
+        </h2>
+        <hr>
+        <form wire:submit="save">
+            <div class="grid md:grid-cols-5 md:gap-6 mt-5">
+                <div class="mb-5">
+                    <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        نام
+                    </label>
+                    <input type="text" id="first_name" name="first_name" wire:model="form.first_name"
+                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="" required/>
+                </div>
+                <div class="mb-5">
+                    <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        نام خانوادگی
+                    </label>
+                    <input type="email" id="last_name" name="last_name" wire:model="form.last_name"
+                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="" required/>
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        ایمیل
+                    </label>
+                    <input type="email" id="email" name="email" wire:model="form.email"
+                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="address@sample.com" required/>
+                </div>
+                <div class="mb-5">
+                    <label for="personal_code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        کد پرسنلی
+                    </label>
+                    <input type="text" id="personal_code" name="personal_code" wire:model="form.personal_code"
+                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="" required/>
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        رمز عبور
+                    </label>
+                    <input type="password" id="password" name="password" wire:model="form.password"
+                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="فقط در صورت نیاز به تغییر رمز پر شود" required/>
+                </div>
+            </div>
+            <div class="text-center">
+                <button type="submit"
+                        class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                    به روز رسانی
+                </button>
+            </div>
+        </form>
     </div>
 </div>
